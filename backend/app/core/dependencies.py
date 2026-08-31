7112fc4c2650de3294e3f3146861b2e508f4f526
itsdhruvmak <129657671+itsdhruvmak@users.noreply.github.com>
2026-08-31 09:57:57 +0530

    changed URL of vercel

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -9,7 +9,7 @@ from app.models.user import User
 
 security = HTTPBearer()
 
-def get_current_user(
+def get_current_user( 
         credentials: HTTPAuthorizationCredentials = Depends(security),
         db: Session = Depends(get_db)
 ) -> User:
